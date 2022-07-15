0576a7f73082cd12763a6e83ec10c6d15f2b14a1
Andrei <andrei.diaconeasa@gmail.com>
2022-07-15 12:00:26 +0300

    skeleton

---
+++ src/test/java/com/config/cassandra/CassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
